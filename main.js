c86766dc0be3e523bb149a4442992fec8b669582
cbdyzj <cbdyzj@jianzhao.org>
2021-06-01 23:32:18 +0800

    Add Electron

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        show: false,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show()
+    })
+
+    const loadFilePromise = mainWindow.loadFile('index-standalone.html')
+
+    // mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
