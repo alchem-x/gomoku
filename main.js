910391071a4645301e3e264ad8e1e772b17e3842
cbdyzj <cbdyzj@jianzhao.org>
2021-06-06 21:17:11 +0800

    Update Initial Window Size

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const path = require('path')
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 800,
+        width: 1200,
+        height: 840,
         show: false,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
